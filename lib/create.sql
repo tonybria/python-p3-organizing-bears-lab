e629373bb7e74b0ee26458112c58cfac959ea9a8
tony-brian <btoni3823@gmail.com>
2023-09-03 17:49:41 +0300

    completed assignment

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  age INTEGER,
+  sex TEXT,
+  color TEXT,
+  temperament TEXT,
+  alive BOOLEAN
+);
---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+-- Insert bears
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES
+  ('Mr. Chocolate', 5, 'M', 'Brown', 'Friendly', TRUE),
+  ('Rowdy', 3, 'M', 'Black', 'Playful', TRUE),
+  ('Tabitha', 4, 'F', 'Gray', 'Shy', TRUE),
+  ('Sergeant Brown', 6, 'M', 'Brown', 'Calm', TRUE),
+  ('Melissa', 2, 'F', 'White', 'Energetic', TRUE),
+  ('Grinch', 7, 'M', 'Green', 'Grumpy', TRUE),
+  ('Wendy', 3, 'F', 'Brown', 'Playful', TRUE),
+  ('unnamed', NULL, NULL, NULL, NULL, NULL);
